39e8d9248c5de16b2d36a15a47b2acc65ad97606
ProgramadorGod <pipenet12@gmail.com>
2023-11-04 18:18:54 -0500

    EmailModule Working perfect

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
+from django.core.mail import EmailMessage
+
 
 
 # Create your views here.
@@ -11,9 +13,14 @@ import json
 @require_POST
 def ContactView(request):
     data = json.loads(request.body.decode('utf-8'))
-    
-    print('Datos recibidos en Django:', data)
+    Name = data["name"]
+    Email = data["email"]
+    Phone = data["phone"]
+    Message = data["message"]
 
-    
+    print('Datos recibidos en Django:', data)
+    Phone = Phone + Name
+    email = EmailMessage(Phone, Message, "pipenet12@gmail.com",[Email])
+    email.send()
 
     return JsonResponse({'Status': 'Ok'})
