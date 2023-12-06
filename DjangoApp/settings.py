77c25f8aff585001d906c0d89bc222774a4da99f
ProgramadorGod <pipenet12@gmail.com>
2023-12-06 15:51:57 -0500

    STARTING THE EVOLUTION

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'UserProducts',
     'USERS',
     'authentication',
+    'Base.apps.BaseConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 CORS_ALLOWED_ORIGINS=[
